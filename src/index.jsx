85f11c3455fb9c540e304b28558bffb999703443
Itz-thevine <divine2christ@yahoo.com>
2022-07-11 18:47:23 +0100

    added modal

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root"),
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import PageWrapper from "../layouts/page_wrapper/PageWrapper";
+import EasyloanModal from "../components/easyloanmodal/EasyloanModal"
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 const LandingPage = () => {
+	const [modalShow, setModalShow] = React.useState(false);
 	return (
 		<PageWrapper>
-			<Container>landing</Container>
+			<Container>
+				landing
+
+				<EasyloanModal/>
+
+			</Container>
 		</PageWrapper>
 	);
 };
