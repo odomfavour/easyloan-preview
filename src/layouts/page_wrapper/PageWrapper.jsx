b97cb73847a1da2a973b3ffa040f3eaba899e753
Cheediogo <bkinetix@gmail.com>
2022-07-12 16:06:16 +0100

    first commit

--- src/layouts/page_wrapper/PageWrapper.jsx
+++ src/layouts/page_wrapper/PageWrapper.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Header } from "../../components/index";
+import Footer from "../../components/Footer";
 
 const PageWrapper = ({ children }) => {
 	return (
 		<>
 			<Header />
 			<section>{children}</section>
-			{/* footer */}
+			<Footer />
 		</>
 	);
 };
