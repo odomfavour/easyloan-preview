75bf61883281430b46cc7bf7dfd2a1e7d1a6458d
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-07-19 19:36:07 +0100

    updates

--- src/components/LoanCalculator.jsx
+++ src/components/LoanCalculator.jsx
@@ -149,6 +149,9 @@ const LoanCalculator = ({ styles }) => {
           .fs-0{
             font-size: 0.5rem;
           }
+          .form{
+            border-radius: 16px;
+          }
 
           // Input Range
           .form-range{}
@@ -168,6 +171,26 @@ const LoanCalculator = ({ styles }) => {
           .form-range::-moz-range-thumb:active {
             background-color: var(--purple);
           }
+
+          .form-select::placeholder{
+            color: #d0d0d0 !important;
+          }
+
+          .form-select::-webkit-input-placeholder {
+            color: #d0d0d0 !important;
+            }
+
+            .form-select:-moz-placeholder { /* Firefox 18- */
+            color: deeppink !important;  
+            }
+             
+            select::-moz-placeholder {  /* Firefox 19+ */
+            color: deeppink !important;  
+            }
+
+            .form-select:-ms-input-placeholder {
+            color: deeppink !important;
+            }
 
 
           // MOBILE VIEW
@@ -194,12 +217,12 @@ const LoanCalculator = ({ styles }) => {
         `}
 			</style>
 
-			<Container className="pb-5 pt-md-4 px-0">
-				<Row className="d-flex align-items-center justify-content-center px-4 px-md-5 form-wrap">
-					<Form className={`rounded col col-lg-4 px-md-5 pt-4 pb-3 ${styles} box-shadow form`}>
+			<Container className="pb-5 pb-md-0 pt-md-4 px-0">
+				<Row className="d-flex align-items-center justify-content-center px-4 px-md-0 form-wrap">
+					<Form className={` col col-lg-4 px-md-5 pt-4 pb-3 ${styles} box-shadow form`}>
 						<Stack>
 							<Form.Group controlId="totalLPO">
-								<Form.Label>Total LPO Amount</Form.Label>
+								<Form.Label className="fw-bold">Total LPO Amount</Form.Label>
 								<Form.Control
 									type="text"
 									name="totalLPO"
@@ -210,14 +233,15 @@ const LoanCalculator = ({ styles }) => {
 							</Form.Group>
 
 							<Form.Group controlId="repaymentPlan">
-								<Form.Label>Repayment Plan</Form.Label>
+								<Form.Label className="fw-bold">Repayment Plan</Form.Label>
 								<Form.Select
 									name="repaymentPlan"
 									value={form.repaymentPlan}
 									onChange={handleChange}
 									aria-label="Repayment Option"
+                  placeholder="Select Repayment Option"
 									className="">
-									<option value="Select your repayment plan" className="d-none">
+									<option value="Select your repayment plan" className="d-none option1">
 										Select your repayment plan
 									</option>
 									<option value="One-off">One-off</option>
@@ -227,7 +251,7 @@ const LoanCalculator = ({ styles }) => {
 							</Form.Group>
 
 							<Form.Group controlId="loanTenure">
-								<Form.Label>Duration/Tenure (Loan)</Form.Label>
+								<Form.Label className="fw-bold">Duration/Tenure (Loan)</Form.Label>
 								<Form.Select
 									aria-label="Repayment Option"
 									name="loanTenure"
@@ -244,7 +268,7 @@ const LoanCalculator = ({ styles }) => {
 							</Form.Group>
 
 							<Form.Group controlId="interestRate">
-								<Form.Label>Interest Rate</Form.Label>
+								<Form.Label className="fw-bold">Interest Rate</Form.Label>
 								<Form.Range
 									name="interestRate"
 									min={0}
@@ -252,7 +276,7 @@ const LoanCalculator = ({ styles }) => {
 									step={1}
 									onChange={handleInterestChange}
 								/>
-								<Container className="d-grid text-end ps-5 pe-3" style={{ fontSize: "8px" }}>
+								<Container className="d-grid text-end ps-5 pe-3 fw-bold" style={{ color: "#000", fontSize: "1rem" }}>
 									<Row>
 										<Col className="p-0"></Col>
 										<Col className="p-0 ">10.5%</Col>
@@ -262,7 +286,7 @@ const LoanCalculator = ({ styles }) => {
 								</Container>
 							</Form.Group>
 
-							<p className="text-center fs-0">
+							<p className="fw-bold">
 								Please note that your terms are subjected to change after review by our Financiers
 							</p>
 
