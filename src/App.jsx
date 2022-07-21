f34394703a8360a55b149c4303f824ac6a8e7ac1
Cheediogo <bkinetix@gmail.com>
2022-07-21 23:15:35 +0100

    profile_page_commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import "./index.css";
 import UserDashboard from "./components/UserDashboard";
 import UserHome from "./pages/UserHome";
 import { LandingPage, LoanCalculatorPage, Faqs, Register, Login } from "./pages/Index";
-
+import UserHomeContent2 from "./components/UserHomeContent2";
+import UserHomeLoan from "./components/UserHomeLoan";
+import UserProfileContent from "./components/UserProfileContent";
 
 function App() {
 	return (
@@ -15,6 +17,9 @@ function App() {
 			<Route path="/faqs" element={<Faqs />} />
 			<Route path="/dashboard" element={<UserDashboard />} >
 				<Route path="/dashboard/user" element={<UserHome />} />
+				<Route path="/dashboard/user2" element={<UserHomeContent2 />} />
+				<Route path="/dashboard/user3" element={<UserHomeLoan />} />
+				<Route path="/dashboard/profile" element={<UserProfileContent />} />
         	</Route>
 			<Route path="/register" element={<Register />} />
 			<Route path="/login" element={<Login />} />
---
+++ src/components/LoanHistoryTable.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Table from 'react-bootstrap/Table';
+
+const LaonHistoryTable = () => {
+  return (
+    <Table responsive>
+      <tbody>
+        <tr>
+            <td>2000 packs of Lonart Drugs </td>
+            <td>Jan 2022 - May 2022 </td>
+            <td>#520,000</td>
+            <td>Closed</td>
+        </tr>
+        <tr>
+            <td>2000 packs of Lonart Drugs </td>
+            <td>Jan 2022 - May 2022 </td>
+            <td>#520,000</td>
+            <td>Closed</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default LaonHistoryTable;
