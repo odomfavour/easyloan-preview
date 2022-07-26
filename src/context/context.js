6ecefefe9b6e3d1049db59badea0b03d8bf457ba
whitechapel007 <ebukareed@gmail.com>
2022-07-26 19:22:17 +0100

    updated

---
+++ src/context/context.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	// const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+
+	//put your states here
+	const [state, setState] = useState();
+
+	const inputs = {
+		state,
+		setState,
+
+		//add your states here
+	};
+	return (
+		<>
+			<AppContext.Provider value={inputs}>{children}</AppContext.Provider>
+		</>
+	);
+};
+
+const useAppContext = () => {
+	return useContext(AppContext);
+};
+export { AppContext, AppProvider, useAppContext };
