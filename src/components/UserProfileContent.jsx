09392c6a061caca6c24847b0dc6c93d32d2e3ea2
Cheediogo <bkinetix@gmail.com>
2022-07-28 15:26:10 +0100

    update on profile_dashboard

--- src/components/UserProfileContent.jsx
+++ src/components/UserProfileContent.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Button, Row, Col, Card } from "react-bootstrap";
 import ProfileProgressBar from "../components/ProfileProgressBar"
+import { Link } from "react-router-dom";
 // import img from "../assets/img.jpg";
 
 const UserProfileContent = () => {
@@ -36,14 +37,18 @@ const UserProfileContent = () => {
                     <div className='bg-gray p-3 profile-section-one'>
                         <div className="d-flex justify-content-between">
                             <img src={user.photoURL} alt="img" className="profile-img "/>
-                            <div className=''><Button >Edit Profile</Button></div>
+                            <div className=''>
+                                <Link to="/detail">
+                                    <Button >Edit Profile</Button>
+                                </Link>
+                            </div>
                         </div>
                         <div>
                             <div className="pt-3 profile-mid-font fw-bold">{user.name}</div>
                             <div className="py-2 profile-small-font" >Business Owner</div>
                             <div className="profile-small-font">2, Favour Street, Osapa, Lagos</div>
                             <div className="py-2 profile-small-font">{user.email}</div>
-                            <div className="profile-small-font">+234 803 222 4567</div>
+                            <div className="profile-small-font">{user.phone}</div>
                             <div className="pt-4 pb-2 profile-small-font">Profile Completion</div>
                             <div className="profile-completion profile-small-font">100%</div>
                             <div className='profile-completion-bg me-5 profile-small-font' 
@@ -64,53 +69,30 @@ const UserProfileContent = () => {
                 <div className='mb-3 user-name-font user-profile-font fw-bold biz-profile-pad'>Business Profile</div>
                     <div className='profile-section-two'>
                         <div className="ps-3 scroll-bar">
-                            <Card className='mb-2 border-0 me-3'>
+                        {isBusinessAdded && ( 
+                        <>
+                        {businessInfo.map((business, index) => {
+                                    return(
+                            <Card className='mb-2 border-0 me-3' key={index}>
                                 <Card.Body>
                                     <div className='d-flex justify-content-between'>
-                                        <div className="pt-1 profile-mid-font fw-bold">Lyd & Ste Ltd</div>
+                                        <div className="pt-1 profile-mid-font fw-bold">{business.businessName}</div>
                                         <Button variant="primary">Edit</Button>
                                     </div>
-                                    <div className="profile-small-font">2, Favour Street, Osapa, Lagos</div>
-                                    <div className="py-1 profile-small-font">lydia@lyd&ste.com</div>
-                                    <div className="profile-small-font">+234 803 222 4567</div>
+                                    <div className="profile-small-font">{business.street}</div>
+                                    <div className="py-1 profile-small-font">{business.businessEmail}</div>
+                                    <div className="profile-small-font">{business.phone}</div>
                                 </Card.Body>
                             </Card>
-                            <Card className='mb-2 border-0 me-3'>
-                                <Card.Body>
-                                    <div className='d-flex justify-content-between'>
-                                        <div className="pt-1 profile-mid-font fw-bold">Space Ltd</div>
-                                        <Button variant="primary">Edit</Button>
-                                    </div>
-                                    <div className="profile-small-font">2, Favour Street, Osapa, Lagos</div>
-                                    <div className="py-1 profile-small-font">info@pace.com</div>
-                                    <div className="profile-small-font">+234 803 222 4567</div>
-                                </Card.Body>
-                            </Card>
-                            <Card className='mb-2 border-0 me-3'>
-                                <Card.Body>
-                                    <div className='d-flex justify-content-between'>
-                                        <div className="pt-1 profile-mid-font fw-bold">APPEX Inc</div>
-                                        <Button variant="primary">Edit</Button>
-                                    </div>
-                                    <div className="profile-small-font">2, Favour Street, Osapa, Lagos</div>
-                                    <div className="py-1 profile-small-font">info@lappexinc.com</div>
-                                    <div className="profile-small-font">+234 803 222 4567</div>
-                                </Card.Body>
-                            </Card>
-                            <Card className='mb-2 border-0 me-3'>
-                                <Card.Body>
-                                    <div className='d-flex justify-content-between'>
-                                        <div className="pt-1 profile-mid-font fw-bold">Green Pharmacy</div>
-                                        <Button variant="primary">Edit</Button>
-                                    </div>
-                                    <div className="profile-small-font">2, Favour Street, Osapa, Lagos</div>
-                                    <div className="py-1 profile-small-font">info@greenpharmacy.com</div>
-                                    <div className="profile-small-font">+234 803 222 4567</div>
-                                </Card.Body>
-                            </Card>
+                            )
+                            })}
+                            </>
+                            )}
                         </div>
                         <div>
-                            <Button className=' '>Add a business</Button>
+                            <Link to="/add-business">
+                                <Button className=' '>Add a business</Button>
+                            </Link>
                         </div>
                     </div>
                 </Col>
