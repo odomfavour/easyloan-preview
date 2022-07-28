48f052078ac997f35b2ebdab5adfa6dac6c09f07
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-07-28 12:07:36 +0100

    fix merge

--- src/components/UserHomeContent.jsx
+++ src/components/UserHomeContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { Button, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
@@ -7,18 +7,21 @@ import stak from "../assets/view_loan.svg";
 import profile from "../assets/edit_profile.svg";
 import pen from "../assets/pen.svg";
 
+
+
 const UserHomeContent = () => {
-	const [user, setUser] = useState({});
-	useEffect(() => {
+    const [user, setUser] = useState({});
+    useEffect(() => {
 		setUser(JSON.parse(localStorage.getItem("user")));
 	}, []);
 
     const [isBusinessAdded, setIsBusinessAdded] = useState(false);
-    const [businessInfo, setBusinessInfo] = useState({});
+    const [businessInfo, setBusinessInfo] = useState([]);
     
 
     const getBusinessInfo = () => {
         const data = JSON.parse(localStorage.getItem("businessInfo"));
+        // const businessInfo = Object.values(data);
         setBusinessInfo(data);
         // businessInfo.push(data);
         // console.log(businessInfo)
@@ -80,7 +83,7 @@ const UserHomeContent = () => {
                       </>
                     )} 
                     {!isBusinessAdded && (
-                      <p className='py-3'>You have not added any business.
+                      <p className='py-3'>You have not added any business. 
                         Add a business now to continue.</p>
                     )}
                     <div>
@@ -131,97 +134,35 @@ const UserHomeContent = () => {
 						</Card.Body>
 					</Card>
 				</Col>
-				<Col sm={12} md={6} className=" overflow-width">
-					<div className="bg-gray px-4 pt-5 user-dashboard-content3" style={{ height: "300px" }}>
-						<p className="fw-bold user-dash-color user-small-font ">Your Business</p>
-						{isBusinessAdded && show ? (
-							<>
-								<div className="mb-3 d-flex justify-content-between user-business">
-									<div className="p-3">
-										<div className="user-biz-font2 mb-1">{businessInfo.businessName}</div>
-										<div className="user-biz-font">Credit Score: 0</div>
-									</div>
-									<button className="shadow-none border-1 rounded my-4 me-3 user-btn-bg7 px-3">
-										Edit
-									</button>
-								</div>
-							</>
-						) : (
-							<p className="py-3">
-								You have not added any business. Add a business now to continue.
-							</p>
-						)}
-						<div>
-							<Link to="/add-business">
-								<Button className="shadow-none user-btn-bg2 ">Add a business</Button>
-							</Link>
-						</div>
-					</div>
+                <Col >
+					<Card
+						className="user-card align-items-center text-center border-0 card-padding"
+                        style={{ width: "12rem", height: "12rem" }}>
+						<Card.Img
+						    variant="top"
+						    src={pen}
+							alt="icon"
+							style={{ width: "2.5rem" }}
+							/>
+						<Card.Body>
+						    <Card.Text>Contact Us</Card.Text>
+						</Card.Body>
+					</Card>
 				</Col>
 			</Row>
-			<div className="py-4">
-				<p className="user-dash-color user-small-font fw-bold text-center">Quick Links</p>
-			</div>
-			<div className="userdash-card">
-				<Row xs={1} md={3} lg={5} className=" gap-2 justify-content-center width-100">
-					<Col>
-						<Card
-							className="text-decoration-none text-black user-card align-items-center text-center justify-content-center border-0 card-padding"
-							style={{ width: "12rem", height: "12rem" }}
-							as={Link}
-							to={"/loan-calculator"}>
-							<Card.Img variant="top" src={calc} alt="icon" style={{ width: "2.5rem" }} />
-							<Card.Body>
-								<Card.Text>Loan Calculator</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
-						<Card
-							className="user-card align-items-center text-center border-0 card-padding"
-							style={{ width: "12rem", height: "12rem" }}>
-							<Card.Img variant="top" src={stak} alt="icon" style={{ width: "2.5rem" }} />
-							<Card.Body>
-								<Card.Text>View all Loans</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
-						<Card
-							className="user-card align-items-center text-center border-0 card-padding"
-							style={{ width: "12rem", height: "12rem" }}>
-							<Card.Img variant="top" src={profile} alt="icon" style={{ width: "2.5rem" }} />
-							<Card.Body>
-								<Card.Text>Edit Profile</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
-						<Card
-							className="user-card align-items-center text-center border-0 card-padding"
-							style={{ width: "12rem", height: "12rem", color: "#212529" }} as={Link}
-							to={"/dashboard/contact"}>
-							<Card.Img variant="top" src={pen} alt="icon" style={{ width: "2.5rem" }} />
-							<Card.Body>
-								<Card.Text>Contact Us</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-				</Row>
-			</div>
-			<div className="bg-gray user-dashboard-content5 mt-5 px-4 pt-4">
-				<div className="d-flex justify-content-between">
-					<p className="user-small-font user-dash-color">Loan History</p>
-					<Button className="user-btn-bg5 shadow-none " style={{ width: "120px" }}>
-						View All
-					</Button>
-				</div>
-				<div className="mt-5">
-					<p>You have not taken a loan. Take a loan now to enjoy the experience.</p>
-				</div>
-			</div>
 		</div>
-	);
-};
+        <div className='bg-gray user-dashboard-content5 mt-5 px-4 pt-4'>
+            <div className='d-flex justify-content-between'>
+                <p className='user-small-font user-dash-color'>Loan History</p>
+                <Button className='user-btn-bg5 shadow-none ' style={{width: "120px"}}>View All</Button>
+            </div>
+            <div className='mt-5'>
+                <p>You have not taken a loan. 
+                    Take a loan now to enjoy the experience.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
 
-export default UserHomeContent;
+export default UserHomeContent
